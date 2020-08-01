4cffbdcf283a76b164ce0180c605c5baf145c41b
Alexander Romanov <aromanov07@gmail.ru>
2020-08-01 23:44:38 +0300

    Update

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,10 +2,10 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return "It works!"
+        return "It works! Realy e"
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.get("hellol") { req -> String in
+        return "Hello, world! azaza"
     }
 }
